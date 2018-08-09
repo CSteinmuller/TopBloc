591acad781b6ec776d463e1765b45b8097b80fe8
Christopher Steinmuller <chris.dungeonmaster@gmail.com>
2018-08-09 17:42:13 -0500

    ?

--- App.java
+++ App.java
@@ -15,22 +15,22 @@ public class App
 {
     public static void main(String[] args) throws IOException
     {
-		ExcelReader dataone = new ExcelReader();
-		ExcelReader datatwo = new ExcelReader();
+	    ExcelReader dataone = new ExcelReader();
+	    ExcelReader datatwo = new ExcelReader();
        
-		dataone.readXcell("../../Downloads/Data1.xlsx");
-		datatwo.readXcell("../../Downloads/Data2.xlsx");
+	    dataone.readXcell("../../Downloads/Data1.xlsx");
+	    datatwo.readXcell("../../Downloads/Data2.xlsx");
 	    
-		ExcelReader datathree = dataone.merge(datatwo);
-		String json = datathree.MakeJSON();
-		CloseableHttpClient client = HttpClients.createDefault();
-		HttpPost httpPost = new HttpPost("http://34.239.125.159:5000/challenge");
-		StringEntity post = new StringEntity(json);
-		httpPost.setEntity(post);
-		httpPost.setHeader("Accept", "application/json");
-		httpPost.setHeader("Content-type", "application/json");
-		CloseableHttpResponse response = client.execute(httpPost);
-		System.out.println(response.getStatusLine().getStatusCode());
-		client.close();
+	    ExcelReader datathree = dataone.merge(datatwo);
+	    String json = datathree.MakeJSON();
+	    CloseableHttpClient client = HttpClients.createDefault();
+	    HttpPost httpPost = new HttpPost("http://34.239.125.159:5000/challenge");
+	    StringEntity post = new StringEntity(json);
+	    httpPost.setEntity(post);
+	    httpPost.setHeader("Accept", "application/json");
+	    httpPost.setHeader("Content-type", "application/json");
+	    CloseableHttpResponse response = client.execute(httpPost);
+	    System.out.println(response.getStatusLine().getStatusCode());	
+	    client.close();
     }
 }
