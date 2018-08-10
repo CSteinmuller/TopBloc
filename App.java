6b2d90734384724ece2c0b839ae943634808f5f3
Christopher Steinmuller <chris.dungeonmaster@gmail.com>
2018-08-10 08:33:57 -0500

    more spacing fixes

--- App.java
+++ App.java
@@ -24,7 +24,6 @@ public class App
 	    ExcelReader datathree = dataone.merge(datatwo);
 	    String json = datathree.makeJSON();
 	    sendRequest(json);
-
     }
 	
      /**
@@ -34,13 +33,13 @@ public class App
      */
     public static void sendRequest(String data) throws IOException
     {
-	CloseableHttpClient client = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://34.239.125.159:5000/challenge");
-        StringEntity post = new StringEntity(data);
-        httpPost.setEntity(post);
-        httpPost.setHeader("Accept", "application/json");
-        httpPost.setHeader("Content-type", "application/json");
-        CloseableHttpResponse response = client.execute(httpPost);
-        client.close();
+	    CloseableHttpClient client = HttpClients.createDefault();
+	    HttpPost httpPost = new HttpPost("http://34.239.125.159:5000/challenge");
+	    StringEntity post = new StringEntity(data);
+	    httpPost.setEntity(post);        
+	    httpPost.setHeader("Accept", "application/json");        
+	    httpPost.setHeader("Content-type", "application/json");        
+	    CloseableHttpResponse response = client.execute(httpPost);        
+	    client.close();
     }
 }
